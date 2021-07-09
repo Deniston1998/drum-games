53408b010c33f6875f6611ffbdcc172b65a927a2
Deniston1998 <85621375+Deniston1998@users.noreply.github.com>
2021-07-09 09:48:47 -0700

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,71 @@
+
+
+
+
+let buttons = document.querySelectorAll(".drum");
+
+
+for (i=0;i<buttons.length;i++){
+
+
+
+buttons[i].addEventListener("click",function(){
+
+   buttonsinnerhtml =this.textContent;
+   
+ makesound(buttonsinnerhtml);
+   
+})
+
+}
+
+
+document.querySelector(".set").addEventListener("keydown",function(e){
+
+
+makesound(e.key);
+
+
+})
+
+
+function makesound(key){  switch (key) {
+   
+   case "w": let crash = new Audio ("sounds/crash.mp3")
+   crash.play();
+      
+      break;
+      
+         case "a": let kick =new Audio ("sounds/crash.mp3")
+         kick.play();
+      
+            break;
+            case "s":let tom4=new Audio ("sounds/tom-4.mp3")
+   tom4.play();
+               break;
+               case "d":let snare =new Audio ("sounds/snare.mp3")
+               snare.play();
+            
+      
+                  break;
+                  case "j":let tom1 =new Audio ("sounds/tom-1.mp3")
+                  tom1.play();
+               
+      
+                     break;
+                     case "k":let tom2 =new Audio ("sounds/tom-2.mp3")
+                     tom2.play();
+                  
+      
+                        break;
+                        case "l":let tom3 =new Audio ("sounds/tom-3.mp3")
+                        tom3.play();
+                     
+      
+                           break;
+                                                                 
+   default: alert("no key");
+      break;
+}
+
+}
